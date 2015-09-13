1a2c97e093153349a8570f890e85039b3829ce13
Andrew Carter <ascarter@icloud.com>
2015-09-13 12:06:43 -0700

    Implement Vendor
    Vendor wrapper using go get

--- goget.go
+++ goget.go
@@ -1,29 +1,62 @@
 package goget
 
-/*
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+)
 
-#!/bin/sh
+// Vendor runs go get into local vendor directory
+func Vendor(pkgs []string) error {
+	// Get the current working directory for the project
+	// TODO: how does this work if you aren't in the project dir?
+	wd, err := os.Getwd()
+	if err != nil {
+		return err
+	}
 
-workingd=`mktemp -d -t goget` || exit 1
-
-# Make sure vendor exists
-# TODO
-
-# Use workingd as the workspace and set src to be vendor
-ln -s `pwd`/vendor ${workingd}/src
-
-GOPATH=${workingd} go get -d -u $*
-#ls -l ${workingd}
-
-# Remove link
-rm ${workingd}/src
-
-*/
-
-func Get(pkgs []string) {
 	// Make a temp workspace
+	ws, err := ioutil.TempDir("", "goget")
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		// TODO: Remove src symbolic link
+		err := os.RemoveAll(ws)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
 	// Create vendor if it doesn't exist
+	v := filepath.Join(wd, "vendor")
+	err = os.MkdirAll(v, 0755)
+	if err != nil {
+		return err
+	}
+
 	// Symlink vendor/src into temp workspace
+	err = os.Symlink(v, filepath.Join(ws, "src"))
+	if err != nil {
+		return err
+	}
+
 	// Run go get -d command
-	// Cleanup
+	cmd := exec.Command("go", "get", "-d")
+	cmd.Args = append(cmd.Args, pkgs...)
+	cmd.Env = append(cmd.Env, fmt.Sprintf("GOPATH=%s", ws))
+	cmd.Env = append(cmd.Env, fmt.Sprintf("PATH=%s", os.Getenv("PATH")))
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err = cmd.Run()
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
